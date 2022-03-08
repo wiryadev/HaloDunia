af6468d8dc7b1d12e914e0260b2b6c87cb127f88
wiryadev <abrarwiryawan@gmail.com>
2022-03-08 20:38:32 +0700

    Initial commit

---
+++ app/src/main/java/com/wiryadev/halodunia/ConstraintActivity.kt
@@ -0,0 +1,11 @@
+package com.wiryadev.halodunia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
---
+++ app/src/main/java/com/wiryadev/halodunia/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.wiryadev.halodunia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val name = "Bambang"
+
+        Log.d("BINARIAN", "Halo $name")
+    }
+}
